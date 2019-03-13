e472c034e854303fc52e2b790d763a7783111673
annabond1001 <annabond1001@gmail.com>
2019-03-13 07:44:30 +0530

    Directory restructure | Wipe module is slides_swipe_render

---
+++ test/test.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '../')
+import wipe
+#TODO make package
+
+wipe.animate(order_input_path='order.csv', video_output_path='output.mp4')
--- utils/generate_sample_swipe_order.py
+++ utils/generate_sample_swipe_order.py
@@ -1,7 +1,9 @@
 import glob
+import sys
 
+frames_dir = sys.argv[1]
 order_file = open('order.csv','w')
 order_file.write("slides, duration, is_primary\n")
-for f in glob.glob('slides/*.jpg'):
+for f in glob.glob('{}/*.jpg'.format(frames_dir)):
     order_file.write("{}, {}, {}\n".format(f, 1, 0))
 order_file.close()
