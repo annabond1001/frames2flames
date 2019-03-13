ed1225a2fcc3b3c64189facd2d8437238d711053
annabond1001 <annabond1001@gmail.com>
2019-03-13 07:51:17 +0530

    removed extra line generated in sample generation for wipe

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # frames2flames
-A framework for animating a series of images, that can be used for presentation purposes.
+A simple framework for animating a series of images, that can be used for presentation purposes.
--- utils/generate_sample_wipe_order.py
+++ utils/generate_sample_wipe_order.py
@@ -4,6 +4,9 @@ import sys
 frames_dir = sys.argv[1]
 order_file = open('order.csv','w')
 order_file.write("slides, duration, is_primary\n")
+lines = []
 for f in glob.glob('{}/*.jpg'.format(frames_dir)):
-    order_file.write("{}, {}, {}\n".format(f, 1, 0))
+    lines.append("{}, {}, {}".format(f, 1, 0))
+    lines.append("\n")
+order_file.writelines(lines[:-1])
 order_file.close()
